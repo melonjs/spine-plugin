818ffa3908ef47d905bad813a96654ec9c00e6fa
Olivier Biot <olivier.biot@me.com>
2023-09-05 09:08:25 +0800

    add inline documentation for the AssetManager class (and merge the init method into the constructor)

--- src/AssetManager.js
+++ src/AssetManager.js
@@ -2,28 +2,50 @@ import { event, video } from "melonjs";
 import * as spineWebGL from "@esotericsoftware/spine-webgl";
 import * as spineCanvas from "@esotericsoftware/spine-canvas";
 
+/**
+ * @classdesc
+ * An Asset Manager class to load spine assets
+ */
 export default class AssetManager {
     asset_manager;
     pathPrefix;
 
+    /**
+     * @param {string} [pathPrefix=""] - a default path prefix for assets location
+     */
     constructor(pathPrefix = "") {
-        event.once(event.VIDEO_INIT, this.initAssetManager.bind(this));
-        this.pathPrefix = pathPrefix;
+        event.once(event.VIDEO_INIT, () => {
+            this.pathPrefix = pathPrefix;
+            if (video.renderer.WebGLVersion >= 1) {
+                this.asset_manager = new spineWebGL.AssetManager(video.renderer.getContext(), this.pathPrefix);
+            } else {
+                // canvas renderer
+                this.asset_manager = new spineCanvas.AssetManager(this.pathPrefix);
+            }
+        });
     }
 
-    initAssetManager() {
-        if (video.renderer.WebGLVersion >= 1) {
-            this.asset_manager = new spineWebGL.AssetManager(video.renderer.getContext(), this.pathPrefix);
-        } else {
-            // canvas renderer
-            this.asset_manager = new spineCanvas.AssetManager(this.pathPrefix);
-        }
-    }
-
+    /**
+     * set a default path prefix for assets location
+     * @see loadAsset
+     * @param {string} pathPrefix
+     */
     setPrefix(pathPrefix) {
-        this.asset_manager.pathPrefix = pathPrefix;
+        this.asset_manager.pathPrefix =  this.pathPrefix = pathPrefix;
     }
 
+    /**
+     * define all spine assets to be loaded
+     * @see setPrefix
+     * @see loadAll
+     * @param {string} atlas
+     * @param {string} skel
+     * @example
+     * // load spine assets
+     * Spine.assetManager.setPrefix("data/spine/");
+     * Spine.assetManager.loadAsset("alien.atlas", "alien-ess.json");
+     * await Spine.assetManager.loadAll();
+     */
     loadAsset(atlas, skel) {
         if (atlas) {
             this.asset_manager.loadTextureAtlas(atlas);
@@ -36,6 +58,10 @@ export default class AssetManager {
         }
     }
 
+    /**
+     * load all defined spine assets
+     * @see loadAsset
+     */
     loadAll() {
         return this.asset_manager.loadAll();
     }
